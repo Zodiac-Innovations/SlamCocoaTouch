4d7e96d8b7718dc50db8cf6f885d7e9032ddb525
Steve Sheets <ssheets@wareto.com>
2017-10-12 20:49:10 -0400

    Initial Commit

---
+++ SlamCocoaTouch/SlamCocoaTouch.h
@@ -0,0 +1,19 @@
+//
+//  SlamCocoaTouch.h
+//  SlamCocoaTouch
+//
+//  Created by Steve Sheets on 10/12/17.
+//  Copyright © 2017 Zodiac Innovations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SlamCocoaTouch.
+FOUNDATION_EXPORT double SlamCocoaTouchVersionNumber;
+
+//! Project version string for SlamCocoaTouch.
+FOUNDATION_EXPORT const unsigned char SlamCocoaTouchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SlamCocoaTouch/PublicHeader.h>
+
+
